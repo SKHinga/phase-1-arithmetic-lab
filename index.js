ffb3a1b2c9a5075ca079b1daa598af2f831240e5
Samuel Hinga <samuelhinga200@gmail.com>
2022-04-09 17:11:03 +0300

    commit tree adf8389c78fbf849001cd130e29f7c202d16a166

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const num1 = 31;
+const num2 = 2;
+const num3 = 19;
+const num4 = 5;
+let multiply = num1 * num2;
+let random = Math.ceil(Math.random());
+let mod = num3%num4;
+let max =   Math.max(2, 3, 5, 7, 12, 20, 17, 19);
 
 // Write your code here
